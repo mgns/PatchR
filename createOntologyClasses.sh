b1849ec3150a5fa74d688161def3efad4446d68f
mgns <magnus@13mm.de>
2015-03-03 11:06:37 +0100

    major revision

--- createOntologyClasses.sh
+++ createOntologyClasses.sh
@@ -1,4 +1,4 @@
-/usr/local/Cellar/jena/2.11.1/bin/schemagen -i http://www.w3.org/ns/prov-o --owl --package de.hpi.patchr.vocab -o ./software/patchr-impl/src/main/java/ -n ProvOntology
-/usr/local/Cellar/jena/2.11.1/bin/schemagen -i ./vocabulary/guo.ttl --owl --package de.hpi.patchr.vocab -o ./software/patchr-impl/src/main/java/ -n GuoOntology
-/usr/local/Cellar/jena/2.11.1/bin/schemagen -i ./vocabulary/patchr.ttl --owl --package de.hpi.patchr.vocab -o ./software/patchr-impl/src/main/java/ -n PatchrOntology
-/usr/local/Cellar/jena/2.11.1/bin/schemagen -i http://rdfs.org/ns/void# -a http://rdfs.org/ns/void# --package de.hpi.patchr.vocab -o ./software/patchr-impl/src/main/java/ -n VoidOntology
+/usr/local/Cellar/jena/2.12.1/bin/schemagen -i http://www.w3.org/ns/prov-o --owl --package de.hpi.patchr.vocab -o ./software/patchr-api/src/main/java/ -n ProvOntology
+/usr/local/Cellar/jena/2.12.1/bin/schemagen -i ./vocabulary/guo.ttl --owl --package de.hpi.patchr.vocab -o ./software/patchr-api/src/main/java/ -n GuoOntology
+/usr/local/Cellar/jena/2.12.1/bin/schemagen -i ./vocabulary/patchr.ttl --owl --package de.hpi.patchr.vocab -o ./software/patchr-api/src/main/java/ -n PatchrOntology
+/usr/local/Cellar/jena/2.12.1/bin/schemagen -i http://rdfs.org/ns/void# -a http://rdfs.org/ns/void# --package de.hpi.patchr.vocab -o ./software/patchr-api/src/main/java/ -n VoidOntology
--- software/patchr-impl/src/main/java/de/hpi/patchr/api/PatchrData.java
+++
@@ -1,9 +0,0 @@
-package de.hpi.patchr.api;
-
-public class PatchrData {
-
-	public void addPatch(Patch patch) {
-		
-	}
-	
-}
